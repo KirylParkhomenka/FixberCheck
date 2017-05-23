3422adf75a7c9d3fe85d2a32df2c1fb25a3ed280
Kiryl <kirjan3@gmail.com>
2017-05-23 16:52:40 +0300

    Change xpath locator for new project item

--- src/test/java/pages/MenuPage.java
+++ src/test/java/pages/MenuPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 
 public class MenuPage extends BasePage{
 
-    private static final By NEW_PROJECT_ITEM = By.xpath("//a[@title='Betty Robbin v1.1']");
+    private static final By NEW_PROJECT_ITEM = By.xpath("//div[@class='h-menu-in']/div/font");
 
     public MenuPage(WebDriver driver) {
         super(driver);
